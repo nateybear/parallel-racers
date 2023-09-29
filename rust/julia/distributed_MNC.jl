fc7f14b4bbe850d389a1abdc847799279d865a0c
Nathan Gardner Hattersley <nhattersley@utexas.edu>
2023-09-29 13:05:51 -0500

    wow look how fast distributed

---
+++ rust/julia/distributed_MNC.jl
@@ -0,0 +1,26 @@
+using Distributed
+@everywhere begin
+    using Statistics
+    using LinearAlgebra
+
+    capN = 100000;
+    capJ = 10;
+    capS = 1000;
+    x = rand(capJ, 1); # product characteristic
+    draws = rand(1, capS); #sim draws
+end
+
+ws = rand(capN);  #agent characteristic
+ys = ceil.(Int, capJ.*rand(capN)); #agent choices (fake - Im not actually simulating choices according to model)
+
+individuals = collect(zip(ws, ys))
+
+function L(b)
+    @sync @distributed (+) for (w, y) in individuals
+        CCPs = @. exp(b[1] + x * (b[2] + b[3] * w + b[4] * draws))
+        CCPs = CCPs ./ sum(CCPs, dims = 1)
+        CCPs = mean(CCPs, dims = 2)
+        choices = 1:capJ .== y
+        sum(@. choices * log(CCPs) + (1 - choices) * log(1 - CCPs))
+    end
+end
