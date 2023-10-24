9b1f71193a386b162e9acbcfa5533d0420ec365f
justinlatona <99098299+justinlatona@users.noreply.github.com>
2023-10-24 17:34:33 -0500

    numba mnc tests

--- rust/python/MNC/py_mnc_numba.py
+++ rust/python/MNC/py_mnc_numba.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numba import jit
+from numba import njit
 import timeit
 
 '''
@@ -9,11 +10,11 @@ so cannot replace np.count or sum to replace mean by mat-multiplying that dim by
 only option is to collapse consumers and choices into a single vector s.t. we then have 2D tensor with sim draws
 '''
 ## 100,000 consumers (each with scalar characteristic W)
-N_cons = 1000
+N_cons = 100000
 ## 10 choices (each with scalar product characteristic X)
 N_choices = 10
 ## 1000 sim draws (S)
-N_sims = 100
+N_sims = 1000
 
 
 ## Then for some arbitrary fixed parameters beta_0, beta_1, beta_2, 
@@ -24,9 +25,9 @@ N_sims = 100
 generate data
 '''
 ## consumer chars (100k) 
-W = np.random.uniform(0,1,N_cons)[:,None]
+W = np.random.uniform(0,1,N_cons)
 ## product chars (10) (in a real implementation, these should be sorted by product IDs 1-10, so they correspond with correct rows in the choices matrix)
-X = np.random.uniform(0,1,N_choices)[None,:]
+X = np.random.uniform(0,1,N_choices)[:,None]
 ## random utility (1000 draws)
 S = np.random.uniform(0,1,N_sims)[None,:]
 ## fake choices Y, corresponding to product IDs (for the 100k consumers, no explicit outside option for now)
@@ -43,7 +44,7 @@ choices[aux==1] = 1
 
 ## generate consumer*simulant by consumer matrix
 ## (NS x N) to be right multiplied onto the product by consumer*simulant matrix (X x NS)
-ns_n = np.zeros((N_cons*N_sims,))
+# ns_n = np.zeros((N_cons*N_sims,))
 
 '''
 fake parameters
@@ -58,75 +59,59 @@ beta = np.random.uniform(0,1,4)
 
 '''
 compute CCPs and total log likelihood across choices and consumers
+--- numba decorators have to immediately precede function
+--- numba does not accept matmul, only @
+'''
+## numba doesn't support axis calls (e.g. in np.sum)
+## can't do matrix-vector multiplication to generate sums because numba does not accept 3D arrays for matrix multiplication
+## see https://numba.readthedocs.io/en/0.51.2/reference/numpysupported.html
+
+
+
+# @jit(nopython=True, parallel=True)
+# @njit(parallel=True)
+# @jit(parallel=True)
+@jit(nopython=True)
+def LL_jit(beta,W,X,S,choices):
+    LL = 0
+    ## loop over consumers and consumer characteristics
+    for i in range(W.shape[0]):
+        e_util_ijs = np.exp(beta[0] + (beta[1] + beta[2]*W[i] + beta[3]*S)*X)
+        ## replace sum and mean with matmul by vectors of ones since numba does not accept axis calls
+        # e_util_is_sumj = np.matmul(np.transpose(e_util_ijs),np.ones(e_util_ijs.shape[0])[:,None]).flatten()
+        # e_util_is_sumj = (np.transpose(e_util_ijs)@np.ones(e_util_ijs.shape[0])[:,None]).flatten()
+        ## make array contiguous (C (row) order) because transpose flips to column (Fortran) order
+        e_util_is_sumj = (np.ascontiguousarray(np.transpose(e_util_ijs))@np.ones(e_util_ijs.shape[0])[:,None]).flatten()
+        CCP_ijs = e_util_ijs/e_util_is_sumj[None,:]
+        ## integrate-out simulants
+        # CCP_ij = np.matmul(CCP_ijs,np.ones(CCP_ijs.shape[1])[:,None]) / CCP_ijs.shape[1]
+        CCP_ij = (CCP_ijs@np.ones(CCP_ijs.shape[1])[:,None]) / CCP_ijs.shape[1]
+        choice_ij = choices[:,i]
+        loglike = -1*np.sum(np.log(choice_ij[:,None]*CCP_ij + (1-choice_ij[:,None])*(1-CCP_ij)))
+        ## cum sum total LL
+        LL += loglike
+    return LL
+
+## start timer
+start = timeit.default_timer()
+
+
+total_LL = LL_jit(beta,W,X,S,choices)
+
+## timer stop and print runtime
+stop = timeit.default_timer()
+print('Time: ', (stop - start), 'seconds')
+print(total_LL)
+
+'''
+Time to run:
+Serial (uses only one core): 52 seconds
+@jit(nopython=True) (still only one core, looks like maybe a tiny bit of two others):  49-50 seconds
+@jit(nopython=True, parallel=True): gives error of sizes of S, X not matching
+@njit(parallel=True): same error
+@jit(parallel=True): same error
 '''
 
-# ## the following does not accept @jit decorator because numba does not allow axis calls
-# def LL(beta,W,X,S,choices):
-#     e_util_ijs = np.exp(beta[0] + (beta[1] + beta[2]*W + beta[3]*S)*X)
-#     ## generate CCP for each consumer and choice (and sim draw)
-#     CCP_ijs = e_util_ijs/np.sum(e_util_ijs,axis=0)
-#     ## integrate-out simulants
-#     CCP_ij = np.mean(CCP_ijs,axis=2)
-#     return -1*np.sum(np.log(choices*CCP_ij + (1-choices)*(1-CCP_ij)))
 
-# ## start timer
-# start = timeit.default_timer()
-
-# total_LL = LL(beta,W,X,S,choices)
-
-# ## timer stop and print runtime
-# stop = timeit.default_timer()
-
-# print('Time: ', (stop - start), 'seconds')
-# print(total_LL)
-
-
-## modified version to accept @jit
-## nevermind, can't do matrix-vector multiplication to generate sums because numba does not accept 3D arrays for matrix multiplication
-## see https://numba.readthedocs.io/en/0.51.2/reference/numpysupported.html
-## need to collapse to 2D array (collapse consumers and product choices dimensions into one)
-## numba does not accept matmul, only @
-## supports flatten but only order C (row major)
-
-## try jax.jit -- seems to support
-
-# @jit(nopython=True)
-# @jit(parallel=True)
-# @jit(nopython=True)
-def LL_jit(beta,W,X,S,choices):
-    ## to allow for jit compilation through numba, add the W and S components then flatten, and combine with the X component
-    ## use vector which is 1 for each consumer and 0 for everyone else to get mean over simulants
-    inner = beta[1] + beta[2]*W + beta[3]*S
-    # e_util_ijs = np.exp(beta[0] + (inner)*X)
-    # ## replace sum of exponentials with matrix multiplication on dimension X by vector of 1s of length X
-    # CCP_ijs = e_util_ijs/np.sum(e_util_ijs,axis=0)
-    # e_util_sum_axis_pre = np.transpose(e_util_ijs)
-    # e_util_sum_axis_0 = np.matmul(np.transpose(e_util_ijs),np.ones((e_util_ijs.shape[0],1)))
-    
-    # test = np.moveaxis(e_util_ijs,0,2)
-    # CCP_ij = np.mean(CCP_ijs,axis=2)
-    # return -1*np.sum(np.log(choices*CCP_ij + (1-choices)*(1-CCP_ij)))
-#     return e_util_ijs, CCP_ijs,e_util_sum_axis_pre, e_util_sum_axis_0
-    return inner
-
-# e_util_ijs, CCP_ijs,e_util_sum_axis_pre,e_util_sum_axis_0 = LL_jit(beta,W,X,S,choices)
-inner = LL_jit(beta,W,X,S,choices)
-
-
-
-A = np.ones((100000,100))
-B = np.ones((1000,100))
-F = np.ones((10,100,1000))
-
-@jit(nopython=True)
-def test_num(A,B,F):
-    # C = A@B
-    # D = np.matmul(A,B)
-    C = F@B
-    return C
-
-C = test_num(A,B,F)
-
-Q = F.flatten()
 
 
