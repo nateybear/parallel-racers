d73fbde7562f9f8c78ee47419cd3ddeaae47c565
Nathan Hattersley <nateybear@users.noreply.github.com>
2024-01-09 12:21:19 -0600

    reshape distributed_MNC.jl to follow DGP

--- multinomial/julia/distributed_MNC.jl
+++ multinomial/julia/distributed_MNC.jl
@@ -1,8 +1,8 @@
 function L(b)
     @sync @distributed (+) for (w, y) in eachrow(mat)
         CCPs = @. exp(b[1] + x * (b[2] + b[3] * w + b[4] * draws))
-        CCPs = CCPs ./ sum(CCPs, dims = 1)
-        CCPs = mean(CCPs, dims = 2)
+        CCPs = CCPs ./ sum(CCPs, dims = 2)
+        CCPs = reshape(mean(CCPs, dims = 3), :)
         choices = 1:capJ .== y
         sum(@. choices * log(CCPs) + (1 - choices) * log(1 - CCPs))
     end
