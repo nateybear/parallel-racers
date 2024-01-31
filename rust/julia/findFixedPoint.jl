054dbdb5b43993e58f73654891dfce08eefa637c
Nathan Hattersley <nate.hattersley@gmail.com>
2024-01-31 09:53:18 -0500

    Lazy fixed point

--- rust/julia/findFixedPoint.jl
+++ rust/julia/findFixedPoint.jl
@@ -1,24 +1,18 @@
 using LinearAlgebra
 using Statistics
 using Distributions: Logistic, cdf
-using Pipe
+using Lazy
+using PartialFunctions
 
-function findFixedPoint((θ, R), tol=1e-7)
-    X = 1:5 # state space
-
-    V₀ = V₁ = zeros(size(X))
-
-    V(v₀, v₁) = max.(v₀, v₁) # value function
-
-    V_replace() =
+function contract((V₀, V₁); θ, R, X)
+    V_replace =
         let v₀ = exp(V₀[1]),
             v₁ = exp(V₁[1])
 
-            @pipe (R + 0.9 * (0.5772 + log(v₀ + v₁))) |>
-                  fill(_, size(X),)
+            @> (R + 0.9 * (0.5772 + log(v₀ + v₁))) fill(size(X))
         end
 
-    V_keep() =
+    V_keep =
         let x = min.(X .+ 1, 5),
             v₀ = exp.(V₀[x]),
             v₁ = exp.(V₁[x])
@@ -26,14 +20,25 @@ function findFixedPoint((θ, R), tol=1e-7)
             @. θ * x + 0.9 * (0.5772 + log(v₀ + v₁))
         end
 
-    while true
-        W₀, W₁ = V_keep(), V_replace()
+    V_keep, V_replace
+end
 
-        if norm(V(V₀, V₁) - V(W₀, W₁)) < tol
-            return W₀, W₁
-        end
+V(v₀, v₁) = max.(v₀, v₁) # value function
 
-        V₀, V₁ = W₀, W₁
+distance(((V₀ₙ, V₁ₙ), (V₀ₙ₊₁, V₁ₙ₊₁))) = norm(V(V₀ₙ, V₁ₙ) .- V(V₀ₙ₊₁, V₁ₙ₊₁))
+
+function findFixedPoint((θ, R), tol=1e-7)
+    X = 1:5 # state space
+
+    V₀ = V₁ = zeros(size(X))
+
+    contractions = iterated(contract $ (; θ, R, X), (V₀, V₁))
+
+    @>> contractions begin
+        partition(2) # make windows of 2
+        takeuntil((<)(tol) ∘ distance) 
+        last # last window of 2
+        last # last element of window
     end
 end
 
